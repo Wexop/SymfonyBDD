7187e46a009296859332ec50b2daebbe86b973c6
Wexop <mattheurtin@gmail.com>
2022-09-26 11:33:32 +0200

    connect bdd

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use Doctrine\Persistence\ManagerRegistry;
+use MongoDB\Driver\Manager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoriesController extends AbstractController
 {
-    #[Route('/', name: 'app_categories')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(ManagerRegistry $doctrine): Response
     {
+
+        //Pour aller chercherles catégories, je vais utiliser un repository
+        //pour me servir de doctrine j'ajoute le paramètre $doctrine à la méthode
+
+        $repo = $doctrine->getRepository(Categorie::class);
+        $categories=$repo->findAll();
+
         return $this->render('categories/index.html.twig', [
-            'controller_name' => 'CategoriesController',
+            "categories"=>$categories
         ]);
     }
 }
