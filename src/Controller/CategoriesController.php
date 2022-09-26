73ac681368317405d59f07842bad2e3007765260
Wexop <mattheurtin@gmail.com>
2022-09-26 11:15:50 +0200

    delete home and add Categories controller

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class CategoriesController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_categories')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('categories/index.html.twig', [
+            'controller_name' => 'CategoriesController',
         ]);
     }
 }
