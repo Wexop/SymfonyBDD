e57059d95f70a9885b6adfd3f4035f01b94b7058
Wexop <mattheurtin@gmail.com>
2022-10-05 12:21:28 +0200

    can change / suppr chaton

--- src/Controller/ChatonsController.php
+++ src/Controller/ChatonsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Categorie;
 use App\Entity\Chaton;
+use App\Form\ChatonSupprimerType;
 use App\Form\ChatonType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -70,6 +71,92 @@ class ChatonsController extends AbstractController
         return $this->render("chatons/voirChatons.html.twig", [
             "categorie" => $categorie,
             "chatons" => $chatons
+        ]);
+
+
+    }
+
+    #[Route('/chatons/modifierChaton/{id}', name: 'modifier_chaton')]
+    public function modifierCaton($id, ManagerRegistry $doctrine, Request $request)
+    {
+        //Récupérer la catégorie dans la BDD
+        $chaton = $doctrine->getRepository(Chaton::class)->find($id);
+
+        //si on n'a rien trouvé -> 404
+
+        if (!$chaton) {
+            throw $this->createNotFoundException("Aucun chaton avec l'id $id");
+        }
+
+        //si on arrive la, c'est qu'on a trouvé une catégorie
+        //on crée le formulaire avec (il sera rempli avec ses valeurs)
+        $form = $this->createForm(ChatonType::class, $chaton);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // le handleRequest a rempli notre objet $vcategorieNew
+            // qui n'est plus vide
+            //pour sauvegarder, on va récupérer un entitymanager de doctrine
+            //qui comme son nom l'indique gère les identités
+            $em = $doctrine->getManager();
+            // on lui dit de la ranger dans la bdd
+            $em->persist($chaton);
+
+            //générer l'insert
+            $em->flush();
+
+            //retour à la page d'accueil
+            return $this->redirectToRoute("app_home");
+
+        }
+
+
+        return $this->render("chatons/modifierChaton.html.twig", [
+            "chaton" => $chaton,
+            "formulaire" => $form->createView()
+        ]);
+
+
+    }
+
+    #[Route('/chatons/supprimer/{id}', name: 'chatons_supprimer')]
+    public function supprimerChaton($id, ManagerRegistry $doctrine, Request $request)
+    {
+        //Récupérer la catégorie dans la BDD
+        $chaton = $doctrine->getRepository(Chaton::class)->find($id);
+
+        //si on n'a rien trouvé -> 404
+
+        if (!$chaton) {
+            throw $this->createNotFoundException("Aucun chaton avec l'id $id");
+        }
+
+        //si on arrive la, c'est qu'on a trouvé une catégorie
+        //on crée le formulaire avec (il sera rempli avec ses valeurs)
+        $form = $this->createForm(ChatonSupprimerType::class, $chaton);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // le handleRequest a rempli notre objet $vcategorieNew
+            // qui n'est plus vide
+            //pour sauvegarder, on va récupérer un entitymanager de doctrine
+            //qui comme son nom l'indique gère les identités
+            $em = $doctrine->getManager();
+            // on lui dit de la supprimer de la BDD
+            $em->remove($chaton);
+
+            //générer l'insert
+            $em->flush();
+
+            //retour à la page d'accueil
+            return $this->redirectToRoute("app_home");
+
+        }
+
+
+        return $this->render("chatons/supprimer.html.twig", [
+            "chaton" => $chaton,
+            "formulaire" => $form->createView()
         ]);
 
 
---
+++ src/Form/ChatonSupprimerType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Chaton;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChatonSupprimerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add("OK", SubmitType::class, ["label" => "Supprimer le chaton"]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Chaton::class,
+        ]);
+    }
+}
