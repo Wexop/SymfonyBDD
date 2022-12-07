45959d68d0fe8ef85aa3529776d7e663ca07f2fc
Wexop <mattheurtin@gmail.com>
2022-12-07 16:01:41 +0100

    add entity and migrate proprietaire

--- src/Controller/ProprietairesController.php
+++ src/Controller/ProprietairesController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Proprietaire;
+use App\Form\ProprietaireType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProprietairesController extends AbstractController
 {
     #[Route('/proprietaires', name: 'app_proprietaires')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine, Request $request): Response
     {
+
+        $proprietaire = new Proprietaire();
+        $form = $this->createForm(ProprietaireType::class, $proprietaire);
+
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $doctrine->getManager();
+
+            $em->persist($proprietaire);
+            $em->flush();
+
+
+        }
+
+        $repo = $doctrine->getRepository(Proprietaire::class);
+        $proprietaire = $repo->findAll();
+
         return $this->render('proprietaires/index.html.twig', [
-            'controller_name' => 'ProprietairesController',
+            "proprietaires" => $proprietaire,
+            "formulaire" => $form->createView()
         ]);
     }
 }
---
+++ src/Entity/Proprietaire.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProprietaireRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProprietaireRepository::class)]
+class Proprietaire
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Prenom = null;
+
+    #[ORM\Column]
+    private ?int $chatons_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->Prenom;
+    }
+
+    public function setPrenom(string $Prenom): self
+    {
+        $this->Prenom = $Prenom;
+
+        return $this;
+    }
+
+    public function getChatonsId(): ?int
+    {
+        return $this->chatons_id;
+    }
+
+    public function setChatonsId(int $chatons_id): self
+    {
+        $this->chatons_id = $chatons_id;
+
+        return $this;
+    }
+}
